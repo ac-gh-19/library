01c0a6936d52bcbaba6ddaf31ec4e204838aa240
ac-gh-19 <chanandrew1925@gmail.com>
2025-08-19 16:59:34 -0700

    broke submit form response into reusable functions

--- javascript.js
+++ javascript.js
@@ -1,35 +1,23 @@
+const url = "https://openlibrary.org/search.json?q=test";
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+
 const bookUrl = "https://openlibrary.org/search.json?";
 const coverUrl = "https://covers.openlibrary.org/b/";
 const closeFormBtn = document.querySelector("#closeFormBtn");
 const showFormBtn = document.querySelector("#showFormBtn");
 const bookForm = document.querySelector("#bookForm");
 
+// keeps track of our collection of books to display info
 let library = {
     myBooks: [],
     numBooks: 0,
-    totalPages: 0,
-    readBooks: 0,
-    unreadBooks: 0,
+    numPages: 0,
+    numReadBooks: 0,
 }
-
-async function getData() {
-    let response = await fetch(bookUrl);
-    let data = await response.json();
-    console.log(data);
-}
-
-function getTodayDate() {
-    const today = new Date();
-    const yyyy = today.getFullYear();
-    const mm = (today.getMonth() + 1).toString().padStart(2, '0');
-    const dd = (today.getDate()).toString().padStart(2, '0');
-    return `${yyyy}-${mm}-${dd}`;
-}
-
-document.getElementById("date").value = getTodayDate();
-
-// getData();
-
 
 function Book(title, author, date, pages, read, imgsrc) {
     return {
@@ -42,6 +30,38 @@ function Book(title, author, date, pages, read, imgsrc) {
     }
 }
 
+let defaultCollection = [
+    Book("1984", "George Orwell", "1949-06-08", 328, true, "https://covers.openlibrary.org/b/id/7222246-L.jpg"),
+    Book("The Hobbit", "J.R.R. Tolkien", "1937-09-21", 310, true, "https://covers.openlibrary.org/b/id/6979861-L.jpg"),
+    Book("The Outsiders", "S. E. Hinton", "1967‑04‑24", 192, true, "https://covers.openlibrary.org/b/olid/OL17063415M-L.jpg"),
+    Book("Fahrenheit 451", "Ray Bradbury", "1953‑10‑19", 190, true, "https://covers.openlibrary.org/b/olid/OL31448957M-L.jpg"),
+    Book("The Great Gatsby", "F. Scott Fitzgerald", "1925‑04‑10", 182, true, "https://covers.openlibrary.org/b/olid/OL26491064M-L.jpg"),
+];
+
+function loadDefaultCollection() {
+    defaultCollection.forEach(book => {
+        library.myBooks.push(book);
+        library.numBooks++;
+        if (book.read) library.numReadBooks++;
+        library.numPages += book.pages;
+        addNewBookToDisplay(book);
+    });
+}
+
+loadDefaultCollection();
+
+// need this to display current day value on input form
+function getTodayDate() {
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    const mm = (today.getMonth() + 1).toString().padStart(2, '0');
+    const dd = (today.getDate()).toString().padStart(2, '0');
+    return `${yyyy}-${mm}-${dd}`;
+}
+
+document.getElementById("date").value = getTodayDate();
+
+// creates a book card with book obj and appends it to the display
 function addNewBookToDisplay(book) {
 
     // Create main container
@@ -110,12 +130,40 @@ function addNewBookToDisplay(book) {
     document.getElementById("display").append(bookContainer);
 }
 
+// creates a new book object and updates our library information
 function addBook(title, author, date, pages, read, imgsrc) {
     let newBook = new Book(title, author, date, pages, read, imgsrc);
     library.myBooks.push(newBook);
     library.numBooks++;
+    if (read) library.numReadBooks++;
+    library.numPages += newBook.pages;
     
     addNewBookToDisplay(newBook);
+}
+
+function resetInputForm() {
+    bookForm.title.value = ""; 
+    bookForm.author.value = ""; 
+    bookForm.pages.value = ""; 
+    bookForm.date.value = getTodayDate(); 
+    bookForm.status.value = "Unread"; 
+}
+
+function getBookData(book) {
+    return {
+        title: book.title,
+        author: book.author_name[0],
+        cover: book.cover_i,
+        imgsrc: `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg` || "assets/catpfp.jpg",
+    };
+}
+
+function getFormData(form) {
+    return {
+        date: form.date.value,
+        pages: form.pages.value,
+        read: (form.status.value == "Read"),
+    }
 }
 
 bookForm.addEventListener("submit", async e => {
@@ -132,23 +180,13 @@ bookForm.addEventListener("submit", async e => {
             alert("Sorry! Your book cannot be found");
         } else {
             let book = data.docs[0];
-            let title = book.title;
-            let author = book.author_name[0];
-            let date = bookForm.date.value;
-            let pages = bookForm.pages.value;
-            let read;
-            if (bookForm.status.value == "Unread") {
-                read = false;
-            } else {
-                read = true;
-            }
-            let cover = book.cover_i; 
-            let imgsrc = `https://covers.openlibrary.org/b/id/${cover}-L.jpg`;
+            const { title, author, cover, imgsrc } = getBookData(book);
+            const { date, pages, read } = getFormData(bookForm);
             addBook(title, author, date, pages, read, imgsrc);
             alert("Your book has been added!");
+            resetInputForm();
         }
     } catch (error) {
-        console.log("Error finding book", error);
         alert("There was an error trying to add the book.");
     }
 });
